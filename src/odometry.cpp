e0f1818078177e8caf4fc7f380a56b55c49270f4
anjalis17 <anjalis17@outlook.com>
2021-04-07 21:33:22 -0700

    Cleaned Up Version

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,7 +1,7 @@
 #include "vex.h"
 #include "autonfunctions.h"
 
-//variables
+//fields
 double currentLeft;
 double currentRight;
 double previousLeft;
@@ -10,22 +10,18 @@ double deltaL;
 double deltaR;
 double deltaX;
 double deltaY;
- //double xPos111;
- //double yPos111;
 double linearDistance;
 double head;
 double theta;
 double lradius;
 
-thread positionTracker;
-
 //constants
 const double trackWidth = 4.625;
 
-//getters
-int getPos(double &xPos111, double &yPos111) {
-  //while(true) {
-
+int getPos(double& xPos, double& yPos) {
+  
+  while (true) {
+    // odometryWheelToInch is a constant global to all our program files... its value is (2.75 * M_PI) / 360
     currentLeft = getLeftEncoderRotation() * odometryWheelToInch; 
     currentRight = -getRightEncoderRotation() * odometryWheelToInch; 
     deltaL = currentLeft - previousLeft;
@@ -35,90 +31,40 @@ int getPos(double &xPos111, double &yPos111) {
     lradius = deltaL / theta;
     linearDistance = 2 * (lradius + (trackWidth / 2)) * sin(theta / 2);
 
-    /*Brain.Screen.print("deltaX: ");   
-    Brain.Screen.print(deltaX);
-            Brain.Screen.print("\r\n ");
-
-    Brain.Screen.print("deltaY ");
-    Brain.Screen.print(deltaY); 
-            Brain.Screen.print("\r\n "); */
-
     deltaX = linearDistance * cos(head + theta / 2);
     deltaY = linearDistance * sin(head + theta / 2);
 
-    Brain.Screen.print("Before ");
-
-    /*Brain.Screen.print("deltaX: ");   
-    Brain.Screen.print(deltaX);
-
-    Brain.Screen.print("deltaY ");
-    Brain.Screen.print(deltaY); */
-
-    /* Brain.Screen.print(head); 
-            Brain.Screen.print("\r\n "); */
-
-    /////////////////////////// THIS IS A SOURCE ISSUE -- WHEN WE CHANGE TO DOUBLE, BOTH BEFORE & AFTER PRINT STATEMENTS ARE 0s, SAME 
-    ///////// W/ FLOAT; ALSO, THE SECOND ISSUE IS THAT EVERYTHING RESETS WHEN WE STOP MOVING THE ROBOT
-    int tempX = xPos111;
-    int tempY = yPos111;
+    // Issue #1: When we change the type of these variables (tempX & tempY) to a double or float (as we intend to do), 
+    //            xPos and yPos no longer change (they always remain at 0).
+    // Issue #2: When tempX & tempY are integers as written below, xPos and yPos change when we move the robot; however, (1) all
+    //           precision is lost, and (2) xPos and yPos reset when the robot stops moving. 
+    int tempX = xPos;
+    int tempY = yPos;
 
     head += theta;
+    xPos = tempX + deltaX; // We tried xPos += deltaX, but that didn't work for some reason; 
+                          // thus, we stored its original value in tempX (above) and used that... Same for Y below...
+    yPos = tempY + deltaY;
     
-   // Brain.Screen.print(xPos111);
-   //         Brain.Screen.print("\r\n ");
-   // Brain.Screen.print(yPos111);
-   //         Brain.Screen.print("\r\n ");
-   /* Brain.Screen.print(tempX); 
-            Brain.Screen.print("\r\n ");
-    Brain.Screen.print(tempY); 
-            Brain.Screen.print("\r\n ");
-
-    Brain.Screen.print(deltaX); 
-            Brain.Screen.print("\r\n ");
-    Brain.Screen.print(deltaY); */
-
-    xPos111 = tempX + deltaX;
-    yPos111 = tempY + deltaY;
-    wait(500, msec); 
+    previousLeft = currentLeft;
+    previousRight = currentRight;
     
-  /*  Brain.Screen.print(" After ");
-    Brain.Screen.print(xPos111);
-        Brain.Screen.print("\r\n ");
-    Brain.Screen.print(yPos111);
-
-   //         Brain.Screen.print("\r\n ");
-   // Brain.Screen.print(tempX); 
-   //         Brain.Screen.print("\r\n ");
-   // Brain.Screen.print(tempY); 
-
-    //xPos111 += deltaX;
-    //yPos111 += deltaY;
-    
-
-
-  /* Brain.Screen.print(head);
-            Brain.Screen.print("\r\n "); */
-  /* Brain.Screen.print(xPos111);
-            Brain.Screen.print("\r\n ");
-    Brain.Screen.print(yPos111);
-            Brain.Screen.print("\r\n "); 
-    Brain.Screen.print(temp111);
-            Brain.Screen.print("\r\n "); */
-
-    //wait(100, msec);
-    /*Brain.Screen.print(xPos);
-            Brain.Screen.print("\r\n ");
-    Brain.Screen.print(yPos);
-    wait(500, msec);
-    Brain.Screen.clearLine();
-    
-    Controller.Screen.print(xPos);
-    Controller.Screen.print("\n ");
-    Controller.Screen.print(yPos); */
-    Brain.Screen.clearLine();
+    wait(10, msec); 
+  }
     return 0;
+}
 
-  //} 
+void skills() {
+
+  double xPos = 0;
+  double yPos = 0;
+
+  getPos(xPos, yPos); // We intend to run this line as a thread so that it can update
+                      // xPos & yPos (localized to this skills() function) as the program 
+                      // is running. We plan to reference xPos & yPos frequently in other functions
+                      // throughout our program (by passing them in as parameters) 
+                      // in order to determine the robot's path of motion. 
+                      
 }
 
 /*int setPos(double x, double y) {
@@ -153,28 +99,3 @@ int getPos(double &xPos111, double &yPos111) {
 
   return 0;
 } */
-
-
-void skills() {
-  // 60 second program
-  double xPos = 0;
-  double yPos = 0;
-
-  //int count = 0;
-
-  Brain.Screen.print("skills ");
-
-  while (true) {
-
- // Brain.Screen.print("count: ");
-  //Brain.Screen.print(++count);
- //         Brain.Screen.print("\r\n ");
-
- //   Brain.Screen.print(xPos);
- //   Brain.Screen.print(" ");
- //   Brain.Screen.print(yPos);
-
-    getPos(xPos, yPos); 
-  }
-  
-}
